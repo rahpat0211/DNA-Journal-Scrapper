d78f08c27a0998a25b3158bdcadaaa4a75f59060
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2021-03-06 14:15:28 -0500

    Create get_features.r

---
+++ R Scripts/get_features.r
@@ -0,0 +1,33 @@
+library(rvest)
+library(stringr)
+# Variables
+main_path<- "https://academic.oup.com/"
+page <- read_html(article_url)
+typeof(page) # list
+
+# Request URL
+# Function may not be needed
+getURL <- function(url_str){
+  return(read_html(url_str))
+}
+# Title
+getTitle <- function(html_page){
+  raw_title <- html_node(html_page, "h1.wi-article-title.article-title-main") %>% html_text()
+  return(str_trim(gsub("[\r\n]", " ",raw_title)))
+}
+# Authors
+getAuthors<- function(html_page){
+  return(html_nodes(html_page, "a.linked-name") %>% html_text())
+}
+# Publication Date
+getPubDate <- function(html_page){
+  return(html_node(html_page, "div.citation-date") %>%
+           html_text())
+}
+# DOI
+getDOI <- function(html_page){
+  raw_text <- html_node(page,"div.ww-citation-primary") %>% html_text()
+  vals <- regexpr("https://doi.org/(.*)", raw_text)
+  doi_len <- attr(vals, "match.length")
+  return(substr(raw_text,vals[1], vals[1] + doi_len -1))
+}
