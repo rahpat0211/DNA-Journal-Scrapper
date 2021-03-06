f6cec7bee9e1bb4bd9b04e6951e101f86273956e
apontewill <78515501+apontewill@users.noreply.github.com>
2021-03-06 18:33:28 -0500

    Add files via upload

---
+++ R Scripts/scraper.R
@@ -0,0 +1,38 @@
+library(rvest)
+library(stringr)
+# Request URL
+# Function may not be needed
+getURL <- function(url_str){
+  return(read_html(url_str))
+}
+# Title
+getTitle <- function(html_page){
+  raw_title <- html_node(html_page, "h1.wi-article-title.article-title-main") %>% html_text()
+  return(str_trim(gsub("[\r\n]", " ",raw_title)))
+}
+# Authors
+getAuthors<- function(html_page){
+  return(html_nodes(html_page, "a.linked-name") %>% html_text())
+}
+# Publication Date
+getPubDate <- function(html_page){
+  return(html_node(html_page, "div.citation-date") %>%
+           html_text())
+}
+getAbstract <- function(html_page){
+  return(html_page %>% html_nodes(".abstract")%>% html_text())
+}
+getFullText <- function(html_page) {
+  text <- html_text(html_nodes(html_page, 'h2.section-title,h3.section-title,p.chapter-para, div.block-child-p.js-p-fig-section'))
+  return(text)
+}
+getKeywords <- function(html_page){
+  return(html_node(page,"div.kwd-group") %>% html_text())
+}
+# DOI
+getDOI <- function(html_page){
+  raw_text <- html_node(page,"div.ww-citation-primary") %>% html_text()
+  vals <- regexpr("https://doi.org/(.*)", raw_text)
+  doi_len <- attr(vals, "match.length")
+  return(substr(raw_text,vals[1], vals[1] + doi_len -1))
+}
