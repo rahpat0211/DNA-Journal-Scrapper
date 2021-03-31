f4c362509052354700665c362403462062924a74
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2021-03-30 20:52:16 -0400

    Update PlainText.r

--- DNA Research/PlainText.r
+++ DNA Research/PlainText.r
@@ -10,11 +10,8 @@ myFiles
 dat_csv <- ldply(myFiles, read_csv)
 
 
-#Creates a csv/text file with all the records that have been extracted
-write.csv(dat_csv, file = "DNA Research.csv")
-
+#Creates a text file with all the records that have been extracted
 write.table(dat_csv, file = "DNA Research.txt", sep = "\t", col.names = TRUE)
 
-#Read in the delivered plain text file
 DNA <- read.table("DNA Research.txt")
-
+View(DNA)
